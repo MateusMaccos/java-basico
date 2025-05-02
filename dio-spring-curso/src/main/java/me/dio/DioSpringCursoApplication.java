f2b9fe890b4bf8b7ea7e64fb12d373a2d4fd12bf
Mateus Machado Costa <mateusdoug@hotmail.com>
2025-05-02 19:58:15 -0300

    fix ordem topologica dos fluxos

--- dio-spring-curso/src/main/java/me/dio/DioSpringCursoApplication.java
+++ dio-spring-curso/src/main/java/me/dio/DioSpringCursoApplication.java
@@ -3,11 +3,15 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url="/",description = "Default Server URL")})
 @SpringBootApplication
 public class DioSpringCursoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DioSpringCursoApplication.class, args);
 	}
-
+	
 }
